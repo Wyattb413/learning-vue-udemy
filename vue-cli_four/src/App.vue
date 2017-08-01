d412ef18ff45c0b18ae61d4f8b977881414d1cbb
Wyatt Barnes <wyattb413@gmail.com>
2017-07-31 20:46:13 -0500

    Fourth exercise using vuc_cli - mainly untilizing components and slots, with dynamic component rendering

---
+++ vue-cli_four/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <br>
+                <button @click="activeComponent = 'appBlue' " class="btn btn-primary">Load Blue Template</button>
+                <button @click="activeComponent = 'appGreen' " class="btn btn-success">Load Green Template</button>
+                <button @click="activeComponent = 'appRed' " class="btn btn-danger">Load Red Template</button>
+                <hr>
+                <component :is="activeComponent">
+                    {{ content }}
+                </component>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Blue from './components/Blue.vue';
+    import Green from './components/Green.vue';
+    import Red from './components/Red.vue';
+
+    export default {
+        components: {
+            appBlue: Blue,
+            appGreen: Green,
+            appRed: Red
+        },
+        data() {
+            return {
+                content: "This is some radical content, man!",
+                activeComponent: null
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
